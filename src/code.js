9258ce92494656f510b14c9b5fa146254d58b13a
Nikos Bakogiannis <nikolaos.bakogiannis@crosswind.ch>
2023-08-09 16:10:28 +0200

    first commit

---
+++ src/code.js
@@ -0,0 +1,7 @@
+var map = L.map('map').setView([37.97574980306906, 23.73482453710132], 16); 
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
---
+++ src/data/locations.md
@@ -0,0 +1,6 @@
+
+## Points of interest
+
+name,coord_e,coord_n
+'giasemi',37.973723695682594,23.7280448
+
